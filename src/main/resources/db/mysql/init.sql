620576e7cd9d8eafbaa99197ded046b2f0fac9c0
cuber <190231170@qq.com>
2016-02-15 17:51:22 +0800

    spring   mybatis   support

--- src/main/resources/db/mysql/init.sql
+++ src/main/resources/db/mysql/init.sql
@@ -9,3 +9,20 @@ create table if not EXISTS IDENTITY_STRATAGY(
   LAST_UPDATE_VALUE INTEGER,
   PRIMARY  key(INSTANCE_NAME)
 );
+create table if not EXISTS RESOURCES(
+  ID varchar(36),
+  RESOUCENAME varchar(255),
+  RESOUCETYPE varchar(255),
+  RESOUCE varchar(255),
+  description VARCHAR(255),
+  PRIMARY  key(ID)
+);
+
+create table if not EXISTS ROLES_RESOUCES(
+  ROLE_ID VARCHAR(36),
+  RESOUCE_ID VARCHAR(255)
+);
+TRUNCATE table RESOURCES;
+TRUNCATE table ROLES_RESOUCES;
+insert into RESOURCES(ID, RESOUCENAME, RESOUCETYPE, RESOUCEMATCH,description) VALUES ('resouces1','autoSecurityPage','page','/autopage.htm','自动权限管理');
+insert into ROLES_RESOUCES(ROLE_ID,RESOUCE_ID) VALUES ('ROLE_ADMIN','resouces1');
